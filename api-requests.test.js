e6edc0e47e4936beb8bb88748006e179e025263f
Alaatohamy <alaatohamy4@gmail.com>
2019-10-18 22:35:17 +0200

    Feat: Add mock fetch function to reduce api calls in testing

--- api-requests.test.js
+++ api-requests.test.js
@@ -1,6 +1,15 @@
 const fetch = require("node-fetch");
 const { getPeoplePromise, getPeople } = require("./api-requests");
 
+const mockFetch = jest.fn().mockReturnValue(
+  Promise.resolve({
+    json: () =>
+      Promise.resolve({
+        count: 87
+      })
+  })
+);
+
 describe("Test api call", () => {
   it("getPeople count", () => {
     expect.assertions(2);
@@ -10,13 +19,23 @@ describe("Test api call", () => {
     });
   });
 
-  it("getPeople count", () => {
+  it("getPeoplePromise", () => {
     expect.assertions(4);
     return getPeoplePromise(fetch).then(res => {
       expect(res).toHaveProperty("count");
       expect(res).toHaveProperty("results");
       expect(res.count).toEqual(87);
       expect(res.results.length).toBeGreaterThan(9);
+    });
+  });
+
+  it("getPeople with mock fetch", () => {
+    expect.assertions(4);
+    return getPeople(mockFetch).then(res => {
+      expect(res).toHaveProperty("count");
+      expect(res.count).toEqual(87);
+      expect(mockFetch.mock.calls.length).toBe(1);
+      expect(mockFetch).toBeCalledWith("https://swapi.co/api/people");
     });
   });
 });
